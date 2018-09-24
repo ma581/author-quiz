e909a2d5279d2bcc7b18194ee777ae851a409f14
maku <manoj.kurien@zuhlke.com>
2018-09-24 14:10:21 +0100

    First author

--- src/AuthorQuiz.js
+++ src/AuthorQuiz.js
@@ -1,19 +1,40 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
+import React from 'react';
 import './App.css';
 
-export default class AuthorQuiz extends Component {
-  render() {
+function AuthorQuiz({turnData}) {
+
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
-      </div>
+        <div>
+            <Hero/>
+            <Turn {...turnData}></Turn>
+            <Continue></Continue>
+        </div>
     );
-  }
+
+}
+
+export default AuthorQuiz;
+
+function Hero() {
+    return <div className="jumbotron col-10 offset-1">
+        <h1 className="display-4">Author Quiz</h1>
+        <p className="lead">Select the book written by the author shown</p>
+    </div>
+}
+
+function Turn({author, books}) {
+    return <div className="row turn" style={{backgroundColor: "white"}}>
+        <div className="col-4 offset-1">
+            <img src={author.imageUrl} alt="Author" className="authorimage"/>
+        </div>
+
+        <div className="col-6 list-group">
+            {books.map(title => <a className="list-group-item list-group-item-action" key={title}>{title}</a>)}
+        </div>
+    </div>
+}
+
+function Continue() {
+    return <div></div>
+
 }
