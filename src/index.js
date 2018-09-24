40638d1e3fb12082ca16e92d979d1f65ddc8c178
maku <manoj.kurien@zuhlke.com>
2018-09-24 14:11:24 +0100

    first author

--- src/index.js
+++ src/index.js
@@ -1,8 +1,25 @@
+import 'bootstrap/dist/css/bootstrap.min.css';
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import AuthorQuiz from './AuthorQuiz';
 import registerServiceWorker from './registerServiceWorker';
 
-ReactDOM.render(<AuthorQuiz />, document.getElementById('root'));
+const authors = [
+    {
+        name: 'Mark Twain',
+        imageUrl: 'https://upload.wikimedia.org/wikipedia/commons/2/25/Mark_Twain%2C_Brady-Handy_photo_portrait%2C_Feb_7%2C_1871%2C_cropped.jpg',
+        imageSource: 'Wikipedia Commons',
+        books: ['The Adventures of Huckleberry Finn', 'Life on the Mississippi', 'Roughing it']
+    }
+];
+
+const state = {
+    turnData: {
+        author: authors[0],
+        books: authors[0].books
+    }
+};
+
+ReactDOM.render(<AuthorQuiz {...state}/>, document.getElementById('root'));
 registerServiceWorker();
