dc2aaa46a4fbfcdc2c498cc5562dd018c2bfd4c4
maku <manoj.kurien@zuhlke.com>
2018-09-25 12:13:51 +0100

    quizzing

--- src/AuthorQuiz.js
+++ src/AuthorQuiz.js
@@ -1,41 +1,92 @@
 import React from 'react';
 import './App.css';
 
-function AuthorQuiz({turnData}) {
+class AuthorQuiz extends React.Component {
 
-    return (
-        <div>
-            <Hero/>
-            <Turn {...turnData}></Turn>
-            <Continue></Continue>
-        </div>
-    );
+    constructor(props) {
+        super(props);
+        this.state = {
+            author: this.props.turnData.author,
+            books: this.props.turnData.books,
+            attemptedToAnswer: false,
+            answeredCorrectly: false,
+            correctBook: this.props.turnData.correctBook
+        };
+        this.handleOnClick.bind(this);
+    }
 
+    render() {
+        const turnProps = {
+            author: this.state.author,
+            books: this.state.books,
+            onClick: t => this.handleOnClick(t)
+        };
+
+        return (
+            <div>
+                {this.state.attemptedToAnswer ? <Feedback isCorrect={this.state.answeredCorrectly}/> : <div/>}
+                <Header/>
+                <Turn {...turnProps}/>
+                {this.state.attemptedToAnswer ? <Continue onClick={() => this.continue()}/> : <div/>}
+            </div>
+        );
+    }
+
+    handleOnClick(title) {
+        this.setState({
+            attemptedToAnswer: true,
+            answeredCorrectly: this.state.correctBook === title
+        })
+    };
+
+    continue() {
+        this.setState({attemptedToAnswer: false});
+
+    }
 }
 
 export default AuthorQuiz;
 
-function Hero() {
+function Header() {
     return <div className="jumbotron col-10 offset-1">
         <h1 className="display-4">Author Quiz</h1>
         <p className="lead">Select the book written by the author shown</p>
     </div>
 }
 
-function Turn({author, books}) {
+function Turn({author, books, onClick}) {
     return <div className="row turn" style={{backgroundColor: "white"}}>
         <div className="col-4 offset-1">
             <img src={author.imageUrl} alt="Author" className="authorimage"/>
         </div>
 
         <div className="col-6 list-group">
-            {books.map(title => <a className="list-group-item list-group-item-action"
-                                   key={title}>{title}</a>)}
+            {books.map(title => <li className="list-group-item list-group-item-action"
+                                    key={title}
+                                    onClick={() => onClick(title)}>{title}</li>)}
         </div>
     </div>
 }
 
-function Continue() {
-    return <div></div>
+function Continue({onClick}) {
+    return <button type="button"
+                   className="btn btn-secondary btn-lg btn-block"
+                   onClick={onClick}>Continue the fun!</button>
+}
 
+function Feedback({isCorrect}) {
+    return isCorrect ?
+        <div className="alert alert-success alert-dismissible fade show" role="alert">
+            <strong>Holy guacamole!</strong> You got that right!.
+            <button type="button" className="close" data-dismiss="alert" aria-label="Close">
+                <span aria-hidden="true">&times;</span>
+            </button>
+        </div>
+        :
+        <div className="alert alert-danger alert-dismissible fade show" role="alert">
+            <strong>Sacre Bleu!</strong> You got that wrong!.
+            <button type="button" className="close" data-dismiss="alert" aria-label="Close">
+                <span aria-hidden="true">&times;</span>
+            </button>
+        </div>
 }
--- src/index.js
+++ src/index.js
@@ -4,6 +4,7 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import AuthorQuiz from './AuthorQuiz';
 import registerServiceWorker from './registerServiceWorker';
+import {shuffle, sample} from 'underscore';
 
 const authors = [
     {
@@ -11,15 +12,43 @@ const authors = [
         imageUrl: 'https://upload.wikimedia.org/wikipedia/commons/2/25/Mark_Twain%2C_Brady-Handy_photo_portrait%2C_Feb_7%2C_1871%2C_cropped.jpg',
         imageSource: 'Wikipedia Commons',
         books: ['The Adventures of Huckleberry Finn', 'Life on the Mississippi', 'Roughing it']
+    },
+    {
+        name: 'Brandon Sanderson',
+        imageUrl: 'https://upload.wikimedia.org/wikipedia/commons/7/70/Brandon_Sanderson_sign.jpg',
+        imageSource: 'Wikipedia Commons',
+        books: ['Elantris', 'Mistborn', 'The Way of Kings']
+    },
+    {
+        name: 'Kent Beck',
+        imageUrl: 'https://upload.wikimedia.org/wikipedia/commons/5/55/Kent_Beck_no_Workshop_Mapping_XP.jpg',
+        imageSource: 'Wikipedia Commons',
+        books: ['Test Driven Development', 'Extreme Programming Explained']
+    },
+    {
+        name: 'Harper Lee',
+        imageUrl: 'https://www.latimes.com/resizer/6EJAg9xU-x1zWFRQrBlakXkBtyg=/1200x1200/www.trbimg.com/img-56c8a0bf/turbine/ct-harper-lee-20160220',
+        imageSource: 'Wikipedia Commons',
+        books: ['To Kill a Mockingbird']
     }
 ];
 
 const state = {
-    turnData: {
-        author: authors[0],
-        books: authors[0].books
-    }
+    turnData: getTurnData(authors)
 };
+
+function getTurnData(authors) {
+    const allBooks = authors.reduce((p, c) => p.concat(c.books), []);
+    const fourRandomBooks = shuffle(allBooks).slice(0, 4);
+    const correctAnswer = sample(fourRandomBooks);
+
+    return {
+        books: fourRandomBooks,
+        author: authors.find(a => a.books.some(title => title === correctAnswer)),
+        correctBook: correctAnswer
+    }
+}
+
 
 ReactDOM.render(<AuthorQuiz {...state}/>, document.getElementById('root'));
 registerServiceWorker();
