f4216b9ea96a6a1e8ad4667d810faf451b505e50
maku <manojasho@gmail.com>
2018-10-11 17:05:57 +0100

    Added cypress tests

---
+++ cypress/integration/spec.js
@@ -0,0 +1,15 @@
+describe('My first test', () => {
+    it('should have title Author Quiz', () => {
+        cy.visit('/')
+            .get('.display-4')
+            .should('have.text', 'Author Quiz')
+    });
+
+    it('should make an alert when clicking an answer', () => {
+        cy.visit('/')
+            .get('.col-6 > :nth-child(1)')
+            .click()
+            .get('.alert')
+            .should('have.class', 'alert-danger')
+    });
+});
